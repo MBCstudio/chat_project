14fef260cc9d645dbe89983c7b18cb2c6ef1fee2
Piotr <272995@student.pwr.edu.pl>
2025-03-26 19:04:54 +0100

    UI, username, thread count added

--- app.py
+++ app.py
@@ -4,21 +4,28 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 import threading
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
 
 # Thread-safe lock for managing messages
-message_lock = threading.Lock()
+message_lock = threading.Lock()#safe acess to list of messages (deadlock can be there)
 messages = []
 
 @app.route('/')
 def index():
     return render_template('chat.html')
 
+@app.route('/threads')#to check how many threads are working in the same time
+def thread_count():
+    return f"Aktualna liczba wątków: {threading.active_count()}"
+
+
 # Handle incoming messages
 @socketio.on('message')
 def handle_message(msg):
+    thread_count()#showing active count of threads
     with message_lock:  # Ensure thread-safe access
         messages.append(msg)
         print(f"Message received: {msg}")
